8923930f63a286f41c4ed7410558f376f49b4c27
William Wang <williamw0825@gmail.com>
2024-08-23 12:59:56 +0800

    feat: add chart Release property

--- cmd/kk/pkg/addons/charts.go
+++ cmd/kk/pkg/addons/charts.go
@@ -47,6 +47,7 @@ func debug(format string, v ...interface{}) {
 	}
 }
 
+// InstallChart install helm chart, see https://helm.sh/docs/intro/using_helm/#more-installation-methods
 func InstallChart(kubeConf *common.KubeConf, addon *kubekeyapiv1alpha2.Addon, kubeConfig string) error {
 	actionConfig := new(action.Configuration)
 	var settings = cli.New()
@@ -75,17 +76,16 @@ func InstallChart(kubeConf *common.KubeConf, addon *kubekeyapiv1alpha2.Addon, ku
 
 	var chartName string
 	if addon.Sources.Chart.Name != "" {
-		if addon.Sources.Chart.Repo == "" && addon.Sources.Chart.Path != "" {
-			fmt.Println(addon.Sources.Chart.Repo)
-			chartName = filepath.Join(addon.Sources.Chart.Path, addon.Sources.Chart.Name)
-		} else {
-			chartName = addon.Sources.Chart.Name
-		}
+		chartName = addon.Sources.Chart.Name
+	} else if addon.Sources.Chart.Repo == "" && addon.Sources.Chart.Path != "" {
+		chartName = addon.Sources.Chart.Path
 	} else {
-		logger.Log.Fatalln("No chart name is specified")
+		logger.Log.Fatalln("No chart name or path is specified")
 	}
 
-	args := []string{addon.Name, chartName}
+	releaseName := addon.Sources.Chart.Release
+
+	args := []string{releaseName, chartName}
 
 	client.Install = true
 	client.Namespace = namespace
@@ -103,8 +103,8 @@ func InstallChart(kubeConf *common.KubeConf, addon *kubekeyapiv1alpha2.Addon, ku
 	if client.Install {
 		histClient := action.NewHistory(actionConfig)
 		histClient.Max = 1
-		if _, err := histClient.Run(addon.Name); err == driver.ErrReleaseNotFound {
-			fmt.Printf("Release %q does not exist. Installing it now.\n", addon.Name)
+		if _, err := histClient.Run(releaseName); err == driver.ErrReleaseNotFound {
+			fmt.Printf("Release %q does not exist. Installing it now.\n", releaseName)
 			instClient := action.NewInstall(actionConfig)
 			instClient.CreateNamespace = true
 			instClient.Namespace = client.Namespace
